95714f852dbc359e28195b2053d402910e1c95b8
Blessed Bagor <blessed@bagor.tech>
2024-04-17 16:47:46 +0800

    Add New Learning: Variables

---
+++ script/learn-js.js
@@ -0,0 +1,12 @@
+//Variable with Datatypes String
+let myStringVariable = "Bob";
+
+//Variable with Datatypes Number
+let myNumberVariable = 10;
+
+//Variables with Boolean Datatypes
+let myBooleanVariable = true;
+
+//Variables with Array Datatypes
+let myArrayVariable = [1, "Bob", true, "Alex"];
+

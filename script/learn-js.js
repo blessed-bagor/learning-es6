c81472f8c24d3d4213eb9c2d7d239e73d6cda0a8
Blessed Bagor <blessed@bagor.tech>
2024-04-17 16:51:45 +0800

    Add new learning: Variable with Object Datatypes

--- script/learn-js.js
+++ script/learn-js.js
@@ -10,3 +10,6 @@ let myBooleanVariable = true;
 //Variables with Array Datatypes
 let myArrayVariable = [1, "Bob", true, "Alex"];
 
+//Variable with Object Datatypes
+let myObjectVariables = document.querySelector("h2");
+myObjectVariables.textContent = "This is my Object Variables";
